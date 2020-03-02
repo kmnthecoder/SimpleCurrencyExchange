d72f57b06d3b67f5fb953eb5c354851a3ec9356b
Kevin <metrix.28@gmail.com>
2020-03-01 21:00:24 -0500

    - change around textwatcher

--- app/src/main/java/Adapters/CurrencyAdapter.java
+++ app/src/main/java/Adapters/CurrencyAdapter.java
@@ -32,16 +32,20 @@ public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.Curren
     @Override
     public CurrencyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View mItemView = mInflater.inflate(R.layout.currency_item, parent, false);
-        return new CurrencyViewHolder(mItemView, this);
+        return new CurrencyViewHolder(mItemView, this, new MyCustomEditTextListener());
     }
 
     @Override
     public void onBindViewHolder(@NonNull final CurrencyViewHolder holder, int position) {
         final CurrencyModel mCurrent = mCurrencyList.get(position);
 
+        holder.myCustomEditTextListener.updatePosition(holder.getAdapterPosition());
+
         // Set hardcoded currency codes
         holder.fromCode.setText("To: " + mCurrent.getFromCurrencyCode());
         holder.toCode.setText("From: " + mCurrent.getToCurrencyCode());
+
+        /*
 
         holder.numToConvert.addTextChangedListener(new TextWatcher() {
             @Override
@@ -62,6 +66,8 @@ public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.Curren
                 holder.converted.setText(Double.toString(mCurrent.convertCurrency()));
             }
         });
+
+         */
     }
 
     @Override
@@ -74,16 +80,51 @@ public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.Curren
         private final TextView fromCode, toCode, converted;
         private final CurrencyAdapter mAdapter;
         private final EditText numToConvert;
+        private MyCustomEditTextListener myCustomEditTextListener;
 
-        public CurrencyViewHolder(final View itemView, CurrencyAdapter adapter) {
+
+        public CurrencyViewHolder(final View itemView, CurrencyAdapter adapter, MyCustomEditTextListener myCustomEditTextListener) {
             super(itemView);
             toCode = itemView.findViewById(R.id.tv_to_currency_code);
             fromCode = itemView.findViewById(R.id.tv_from_currency_code);
             converted = itemView.findViewById(R.id.tv_converted_currency);
             numToConvert = itemView.findViewById(R.id.et_convert_amount);
+
+            this.myCustomEditTextListener = myCustomEditTextListener;
+            this.numToConvert.addTextChangedListener(myCustomEditTextListener);
+
             this.mAdapter = adapter;
 
+        }
+    }
 
+    private class MyCustomEditTextListener implements TextWatcher {
+
+        private int position;
+
+        public void updatePosition(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            // convert to hardcoded currency immediately
+            // TO DO: research how the different override methods work
+            //mCurrent.setFromCurrencyVal(Double.parseDouble(holder.numToConvert.getText().toString()));
+            //holder.converted.setText(Double.toString(mCurrent.convertCurrency()));
+
+            //mCurrencyList.get(position).setFromCurrencyVal(Double.parseDouble(holder.numToConvert.getText().toString()));
+            //mCurrencyList.get(position).converted.setText(Double.toString(mCurrent.convertCurrency()));
         }
 
     }
